9bfa31749240c19f792033353c21092d10ef5c7f
prithvikings <prithvi07raj07@gmail.com>
2026-01-04 21:03:32 +0530

    feat: add export for new model in index file

--- backend/models/index.js
+++ backend/models/index.js
@@ -6,3 +6,4 @@ export { default as HealthRecord } from "./healthRecord.models.js";
 export { default as InventoryItem } from "./inventoryItem.models.js";
 export { default as InventoryUsage } from "./inventoryUsage.models.js";
 export { default as FinancialTransaction } from "./financialTransaction.models.js";
+
